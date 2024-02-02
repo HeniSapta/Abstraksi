1c599616c855439325e51cd1a93592446fd023b0
HeniSapta <152345387+HeniSapta@users.noreply.github.com>
2024-02-02 11:09:21 +0700

    Add files via upload

---
+++ tugas11/tugas11/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class Makanan
+{
+    public abstract void Sound();
+}
+
+interface IFlyable
+{
+    void Fly();
+}
+
+public class Cake : Makanan, IFlyable
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Cake strawberry");
+    }
+
+    public void Fly()
+    {
+        Console.WriteLine("Cake strawberry lezat sekali");
+    }
+
+
+public class Gorengan : Cake
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Gorengan the best");
+        }
+    }
+
+public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Makanan makanan = new Gorengan();
+            makanan.Sound();
+            IFlyable flayer = new Cake();
+            flayer.Fly();
+        }
+    }
+}
